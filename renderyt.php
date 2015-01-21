f08752db4ff668e3395dc7b542fa37d6b008baaf
root <root@wmaiz-v-sofa02>
2015-01-21 18:20:05 +0100

    xvfb geht

--- renderyt.php
+++ renderyt.php
@@ -4,15 +4,42 @@
 /*
 INSTALL
 
+wget http://selenium-release.storage.googleapis.com/2.44/selenium-server-standalone-2.44.0.jar
+sudo apt-get update
+sudo apt-get install firefox
+sudo apt-get install xvfb
 
-firefox 
-xvfb
-flash optinal
+
+DEBUG
+apt-get install x11vnc
+x11vnc -display :10
+
+
 
 RUN
 sudo Xvfb :10 -ac  -screen 0 1600x1200x24+32
+sudo Xvfb :10 -ac  -screen 0 400x300x24+32
 export DISPLAY=:10
 java -jar selenium-server-standalone-2.44.0.jar > selenium.log 
+nodejs renderyt.js
+
+
+
+
+ubnutu upstartscript
+--------------------
+nohup java -jar selenium-server-standalone-2.44.0.jar -host 127.0.0.1 -port 4444 -log selenium.log > /dev/null
+sudo iptables -A INPUT -p tcp --dport 4444 -s 127.0.0.1 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 4444 -j DROP
+
+apt-get install daemon
+
+anleitung befolgen: https://github.com/feniix/selenium-grid-startup
+
+
+
+
+
 phpunit renderyt.php
 */
 require_once 'vendor/autoload.php';
