0052cfd01ea95323c861ac7d428052975cf2591d
root <root@wmaiz-v-sofa02>
2015-01-26 19:35:11 +0100

    driverpool

--- headless.js
+++ headless.js
@@ -1,49 +1,26 @@
-function test_selenium(){
-	//check if selenium is running
-	var spawn = require('child_process').spawn,
-	    servicestate = spawn('service', ['selenium', 'status']);
-	var selenium_running = false;
-
-	servicestate.stdout.on('data', function (data) {
-	  selenium_running = (data.toString().indexOf("Selenium Grid Server is not running"))==-1;
-	  console.log("selenium:", selenium_running);
-	  servicestate.kill();
-	  if (!selenium_running) throw new Error("Selenium not running.");
-	});
-
-	servicestate.stderr.on('data', function (data) {
-	  console.log("selenium: State could not be determined. Continuing anyway.");
-	  selenium_running = true;
-	  servicestate.kill();
-	});
-
-	servicestate.on('close', function (code) {
-	  //console.log('child process exited with code ' + code);
-	  servicestate = null;
-	});	
-}
 
 var headless = require('headless');
 
-var options = {
-	display: {width: 1024, height: 980, depth: 24}
-};
-
 module.exports = function runHeadless(myoptions, callback){
+
+	// var options = {
+	// 	display: {width: 1024, height: 980, depth: 24}
+	// };	
 	
 	test_selenium(); //throws error if not running	
 
-	options = myoptions || options;
+	options = myoptions;
 
 	headless(options, function(err, childProcess, servernum) {
-	  // childProcess is a ChildProcess, as returned from child_process.spawn()
-	  console.log('Xvfb running on server number', servernum);
-	  console.log('Xvfb pid', childProcess.pid);
-	  console.log('err should be null', err);
 
-	  if (typeof callback === "function") callback(err, childProcess, servernum);
-	  // console.log('dying in 5 sek');
-	  // setTimeout(function(){process.exit(code=0)},5000);
+		if (!err){
+			// childProcess is a ChildProcess, as returned from child_process.spawn()
+			console.log('Xvfb:', true);
+			if (typeof callback === "function") callback(err, childProcess, servernum);
+		} else {
+			console.log('Xvfb:',err);
+			throw new Error("Xvfb not running.");
+		}
 
 	});
 
@@ -52,8 +29,30 @@ module.exports = function runHeadless(myoptions, callback){
 
 var diretcall = !module.parent;
 if (diretcall){
-	//console.log("diretcall");
-	module.exports();
-} else {
-	//console.log("module call");	
+	throw new Error("diretcall not supported");
+}
+
+function test_selenium(){
+	//check if selenium is running
+	var spawn = require('child_process').spawn,
+	    servicestate = spawn('service', ['selenium', 'status']);
+	var selenium_running = false;
+
+	servicestate.stdout.on('data', function (data) {
+	  selenium_running = (data.toString().indexOf("Selenium Grid Server is not running"))==-1;
+	  console.log("Selenium:", selenium_running);
+	  servicestate.kill();
+	  if (!selenium_running) throw new Error("Selenium not running.");
+	});
+
+	servicestate.stderr.on('data', function (data) {
+	  console.log("Selenium: State could not be determined. Continuing anyway.");
+	  selenium_running = true;
+	  servicestate.kill();
+	});
+
+	servicestate.on('close', function (code) {
+	  //console.log('child process exited with code ' + code);
+	  servicestate = null;
+	});	
 }
--- renderyt.js
+++ renderyt.js
@@ -1,53 +1,124 @@
 
-//node-debug --web-host 0.0.0.0 renderyt.js 
+/*
+node-debug --web-host 0.0.0.0 --save-live-edit true renderyt.js 
+http://wmaiz-v-sofa02.dbc.zdf.de:8080/debug?port=5858
+*/
 
 //starte 2 fb
 
 //per request select fb
+var drivers = {};
+
+//setup environment
+var By = require('selenium-webdriver').By,
+    until = require('selenium-webdriver').until,
+    firefox = require('selenium-webdriver/firefox');
+
 var runHeadless = require('./headless');
 var options = {
-  display: {width: 600, height: 800, depth: 24}
+	//keine unterschiedlichen displaygrößen per request eher per server
+	display: {width: 600, height: 600, depth: 24}
 };
 
 var headless = runHeadless(options,function(err, childProcess, servernum){
 	//xvfb ready
 	if(!err){
-		console.log("display at:",servernum);
+		//console.log("display at:",servernum);
 		process.env.DISPLAY = ":" + servernum;
-		renderrequest();		
+		drivers.driver1 = new firefox.Driver();
+		drivers.driver2 = new firefox.Driver();		
+		initcompleted();
+	} else {
+		//error
+		throw new Error("X or Selenium not running.");
 	}
 });
 
+var waitingCalls = [];
+function initcompleted(){
+	for (var i = waitingCalls.length - 1; i >= 0; i--) {
+		waitingCalls[i]();
+	};
+	waitingCalls = null;
+}
 
-var By = require('selenium-webdriver').By,
-    until = require('selenium-webdriver').until,
-    firefox = require('selenium-webdriver/firefox');
+function pageloaded(driver,posttarget,Timeout,driverNo){
+	clearTimeout(Timeout);
+
+	console.log("session",driverNo,"loaded");
+
+	//posttarget = curl -v -X PUT http://127.0.0.1:5984/albums/6e1295ed6c29495e54cc05947f18c8af/artwork.jpg?rev=2-2739352689 --data-binary @artwork.jpg -H "Content-Type: image/jpg"
+	setTimeout(function (){
+			driver.takeScreenshot().then(
+			    function(image, err) {
+			        require('fs').writeFile('google.png', image, 'base64', function(err) {
+			            console.log("error:",err);
+			            //driver.quit(); not reuseable on quit
+			            //process.exit(code=0);
+			        });
+			    }
+			);	
+			
+		},2000);	
+}
 
 
 
-function renderrequest(){
-	console.log("run renderrequest");
+module.exports = function renderrequest(url,posttarget, driverNo){
 
-	var driver = new firefox.Driver();
+	driver = drivers["driver"+driverNo];
+
+	if (!driver){
+		waitingCalls.push( function ()
+		{
+			var _url = url;
+			var _posttarget = posttarget;
+			var _driverNo = driverNo;
+			renderrequest(_url,_posttarget,_driverNo);
+		} );
+		return;
+	}
+
+	console.log("running on session:", driverNo);
+
+	//todo
+	//laufenden process/timeout abfragen wenn vorhandn dann push nach waitingCalls
+	//alternativ einen pool aus new firefox.Driver(); //bringt keinen performane vorteil lieber eins nach dem anderen
+
+	var url = url || 'http://merlin.intern.zdf.de:5984/twr/c0cb0d515756ec82976722085fa7d904257eb5de/rendersource.html';
+
+	var timeout1 = 0;
 	//driver.get('http://merlin.intern.zdf.de:8811/');
-	driver.get('http://merlin.intern.zdf.de:5984/twr/f4a7e6e2567107a950d86d74af9eea8b41904090/rendersource.html');
+	driver.get(url);
 	//driver.findElement(By.name('q')).sendKeys('webdriver');
 	//driver.findElement(By.name('btnG')).click();
-	setTimeout(function (){
-		driver.takeScreenshot().then(
-		    function(image, err) {
-		        require('fs').writeFile('google.png', image, 'base64', function(err) {
-		            console.log(err);
-		            driver.quit();
-		            //process.exit(code=0);
-		        });
-		    }
-		);	
-		
-	},5000);
+
+	var pagestate = driver.executeScript('return document.readyState;')
+
+	timeout1 = setTimeout(function ()
+	{
+		console.log("session",driverNo,"took to much time to load");
+		//driver.quit();
+		//process.exit(code=0);		
+	},10000)
+
+	pagestate.then(function(readyState){
+		console.log("pagestate",readyState);
+		if (readyState === "complete"){
+			pageloaded(driver, posttarget, timeout1, driverNo);
+		}
+	});
+
+	
+
+
 
 }
 
+
+//debug
+module.exports(0,0,1);
+module.exports(0,0,2);
 
 
 // driver.wait(until.titleIs('webdriver - Google Search'), 1000);
